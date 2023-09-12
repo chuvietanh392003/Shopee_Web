9ede3a7092c8eef82beab83cac25c63005f86c2e
chuvietanh392003 <130541979+chuvietanh392003@users.noreply.github.com>
2023-09-12 23:37:30 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <h1 className='text-center font-bold mt-10 text-green-700'>
+        Hello everybody! I'm React & TailwindCSS component
+      </h1>
+      
+    </>
+  );
+}
+
+export default App;
+
+
